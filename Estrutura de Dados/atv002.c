5e89a0e684d579dcb1b3299c721dbd050f331f60
FalLying <53955982+FalLying@users.noreply.github.com>
2019-08-16 19:23:41 -0300

    Create atv002.c

---
+++ Estrutura de Dados/atv002.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+	float numero = -2.5;
+
+	clrscr();
+	printf("1234567890\n");
+	printf("%7f\n", numero);
+	printf("%7.0f\n", numero);
+	printf("%7.3f\n", numero);
+	printf("%8.3f\n", numero);
+	printf("%9.3f\n", numero);
+	printf("\n", numero);
+	printf("%8.4f\n", numero);
+}
