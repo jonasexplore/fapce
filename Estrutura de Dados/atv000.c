06e560ec0a4d3de542fbb0796d124eb183948fa4
FalLying <53955982+FalLying@users.noreply.github.com>
2019-08-17 10:08:03 -0300

    Create atv000.c

---
+++ Estrutura de Dados/atv000.c
@@ -0,0 +1,5 @@
+1 - Primeiro, é necessário verificar se o local onde o o indivíduo está, dispõe desse tipo de transporte.
+2 - Caso sim, será necessário localizar um ponto de parada que sinalize a parada e um ônibus.
+3 - Será necessário aguarda por um tempo até que o transporte desejado apareça.
+4 - Quando avista-lo, indique com a mão para que o motorista entenda que você quer usa-lo.
+5 - Quando o ônibus parar completamente e abrir a porta, apenas entre.
