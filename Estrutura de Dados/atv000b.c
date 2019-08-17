ff80ed130508a6e84736965e7cd0b1386de561e6
FalLying <53955982+FalLying@users.noreply.github.com>
2019-08-17 10:08:36 -0300

    Create atv000b.c

---
+++ Estrutura de Dados/atv000b.c
@@ -0,0 +1,12 @@
+1 - Será necessário um carro com pneus, independente do estado dele.
+2 - Será necessários a ferramentas,  como macaco e chaves para efetuar a remoção.
+3 - Uma pessoa terá que dispor do seu tempo para trocar o pneu.
+4 - Verifique se o carro está desligado.
+5 - Escolha o pneu que queira remover.
+6 - Utilize a ferramenta indicada para elevar/levantar o carro.
+7 - Com as chaves de remoção, comece a remover os parafusos.
+8 - Depois de removido todos os parafusos, puxe o pneu lentamente em sentido oposto a lataria do carro.
+9 - Pegue o novo pneu e coloque-o no lugar do antigo.
+10 - Com as chaves, novamente, comece a parafusa-lo.
+11 - remova o a ferramenta utilizada para elevar o carro.
+12 - Ligue e teste.
