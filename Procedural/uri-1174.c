a951d137fa7c8c7abb25ba0eb45f7c7865d158f0
FalLying <53955982+FalLying@users.noreply.github.com>
2019-08-20 09:47:43 -0300

    Create uri-1174.c

---
+++ Procedural/uri-1174.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void int main(int argc, char const *argv[])
+{
+	float vet[100];
+
+	for (int i = 0; i < 100; ++i){
+		scanf("%f", %vet[i]);
+	}
+	for (int i = 0; i < 100; ++i)
+	{
+		if (vet[i] <= 10){
+			printf("A[%i] %f\n", i, vet[i]);
+		}
+	}
+	return 0;
+}
